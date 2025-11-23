ca1c644df2ce94794f9e199e643482b93d500e3b
Dhananjay Yadav <ydhananjay2001@gmail.com>
2025-11-23 16:21:58 +0530

    added some changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 // ----------> root rendering of react app with redux provider and persist gate for redux-persist.
-// The Provider component makes the Redux store available to any nested components that need to access the Redux store.
-// The PersistGate delays the rendering of the app's UI until the persisted state has been retrieved and saved to redux.
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={dataStore}>
